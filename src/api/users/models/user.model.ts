c042e89796ea22115e9a00a89e354170c529c436
yuvraj-bytes <upadhyayyuvi@gmail.com>
2024-04-25 19:03:04 +0530

    Update user.model.ts

--- src/api/users/models/user.model.ts
+++ src/api/users/models/user.model.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import { ROLE } from 'src/enum/role.enum';
+import { ROLE } from '../../../enum/role.enum';
 import * as bcrypt from 'bcrypt';
 
 export type UserDocument = HydratedDocument<User>;
